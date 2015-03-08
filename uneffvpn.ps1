d5a65b49f1bbf7350ea1592655f326e4f788550f
Brandon Kvarda <bjkvarda@gmail.com>
2015-03-08 14:42:00 -0700

    First and only

---
+++ uneffvpn.ps1
@@ -0,0 +1,32 @@
+﻿$count = 0
+
+
+
+Function Fix-VPN{
+  
+  $result = Test-Connection www.google.com -Quiet
+  Write-Host $result
+  if($result){
+  
+   Write-Host "Nothing Wrong Here!"
+   exit
+
+  }
+
+  else{
+
+   Write-Host "Restarting VPN"
+   cd 'C:\Program Files\pia_manager'
+   .\pia_manager.exe
+   Start-Sleep -s 60
+   if($count -lt 5){
+     $count++
+     Fix-VPN
+     }
+   else{
+    Write-Host "Tried 5 times, something else is effed"
+   }
+  }
+}
+
+Fix-VPN
